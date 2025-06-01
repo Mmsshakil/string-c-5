4f29dcdb5d149a34744b4504731dd657212713e4
Mmsshakil <softhandshakil@gmail.com>
2025-06-02 05:48:20 +0600

    first commit

---
+++ string.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "shakil shahriar";
+    string s2 = "M M Shahriar Shakil";
+
+    s = "M M Shahriar Shakil";
+    cout << s << endl;
+
+    if (s == s2)
+    {
+        cout << "Same" << endl;
+    }
+    else
+    {
+        cout << "Not Same" << endl;
+    }
+
+    return 0;
+}
