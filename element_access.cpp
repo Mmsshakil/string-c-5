86a0709825cec20bee3a82487317522bb93a7ab4
Mmsshakil <softhandshakil@gmail.com>
2025-06-02 06:23:47 +0600

    string element access

---
+++ element_access.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hello";
+
+    cout << s[1] << endl;
+
+    cout << s.front() << endl; // print first value of string
+    cout << s.back() << endl;  // print last value of string
+
+    return 0;
+}
